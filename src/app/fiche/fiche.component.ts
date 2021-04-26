ae76ab653e66e108a34241c1d46bc59f5aa9f6a2
Mariem <mariem.rouiissi@gmail.com>
2021-04-26 17:50:17 +0200

    Get all fiches

--- src/app/fiche/fiche.component.ts
+++ src/app/fiche/fiche.component.ts
@@ -8,10 +8,45 @@ import { FicheService } from './fiche.service';
 })
 export class FicheComponent implements OnInit {
 
+ public fiche = {
+  'date_butoir': null,
+  'heures_estime': 0,
+  'id': 1,
+  'lieu': null,
+  'name': "Diag. de classe",
+  'note': null,
+  'tags': [],
+  'url': null,
+  'user': null
+} 
+  fiches = [{
+    'date_butoir': null,
+    'heures_estime': 0,
+    'id': 0,
+    'lieu': null,
+    'name': "",
+    'note': null,
+    'tags': [],
+    'url': null,
+    'user': null
+  } ] ;
   constructor(private service:FicheService) { }
 
   ngOnInit(): void {
- 
+    this.service.getFiches().subscribe(data=>{
+      console.log(data);
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      console.log('res ===> ', res.length);
+        this.fiches = []
+      for (let i =0 ; i< res.length ; i++){
+        if (res[i] !== undefined){
+          this.fiches.push(res[i]);
+        }
+     
+      }
+      
+    })
   }
 getAll(){
   this.service.getFiches().subscribe(data=>{
